dd5150f0d2e911749f05949305c48718335b7f01
Richkum <kumatum2@gmail.com>
2023-10-06 12:27:38 +0100

    working out my js functions

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,7 +1,40 @@
-const conv = document.getElementById("conv")
-const disp = document.getElementById("display")
+const start= ()=>{
+  document.querySelector('button').addEventListener('click', HandShake);
+}
 
+const HandShake= ()=>{
+  let number= Number.parseInt(document.myForm.number.value);
+  let shake= document.querySelector('.result');
 
-let rem
-let i = 1
-let ans = " "
+  if (number>0 && number<65){
+    let options= new Array("wink","double blink","close your eyes","jump");
+    let result= [];
+    let rem;
+    let a=0;
+    let reverse
+
+    while(number>0){
+      rem = number%2;
+      if (rem === 1) {
+        if (a === 4) { 
+         reverse= true;
+        } else {
+          result.push(options[a]);
+        }
+      }
+      a++;
+      number = Math.floor(number/2);
+    }
+    
+    
+    if(reverse === true){
+      result.reverse();
+    }
+    
+    shake.textContent = "Handshake: " + result.join(" , ");
+  }
+  else{
+    shake.textContent = "enter a  number between 0 and 64 but not 16";
+  }
+}
+start();
